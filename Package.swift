001d7de1ed7d1efcc0714414c3f06d2c9e942b37
Enzo Sterro <enzo.sterro@gmail.com>
2021-09-02 23:19:25 +0300

    Support iOS

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "WatchLogger",
-    platforms: [.watchOS(.v6), .macOS(.v10_15)],
+    platforms: [.watchOS(.v6), .macOS(.v10_15), .iOS(.v13)],
     products: [.library(name: "WatchLogger", targets: ["WatchLogger"])],
     targets: [.target(name: "WatchLogger", dependencies: [])],
     swiftLanguageVersions: [.v5]
